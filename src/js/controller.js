e694074bc0521028a13ea06d4751e6489063322d
Aleksey <1aleksey.mazur@gmail.com>
2022-09-02 18:29:36 +0300

    DOM updating without rerendering not changed elements implemented; active class added to selected search result

--- src/js/controller.js
+++ src/js/controller.js
@@ -19,14 +19,14 @@ const controlRecipes = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
+    //Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+
     // Loading recipe
     await model.loadRecipe(id);
 
     // Rendering recipe
     recipeView.render(model.state.recipe);
-
-    // TEST
-    // controlServings();
   } catch (err) {
     recipeView.renderError();
   }
@@ -66,7 +66,8 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   // Update the recipe view
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
